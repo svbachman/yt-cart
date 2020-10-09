7c05f00526f464b7937fc0d40e3f9e0b458c3dc5
svbachman <sv.bachman@gmail.com>
2020-10-10 00:34:01 +0300

    2 files modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React,{Component} from "react";
 import "./style.css";
+import data from "./data.json"
+import Products from "./Components/Products"
 
-export default function App() {
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state= {
+      products: data.products,
+      size:"" ,
+      sort:"",
+    }
+  }
+  render(){
   return (
     <div className="grid-container">
       <header>
       <a href="/">React Cart </a>
       </header>
       <main>
-      Product List
+      <div className="content">
+      <div className="main">
+      <Products products={this.state.products} />
+      </div>
+      <div className="sidebar">
+      CartItems
+      </div>
+      </div>
       </main>
       <footer>
       All Rights
@@ -16,4 +34,6 @@ export default function App() {
       
     </div>
   );
+  }
 }
+

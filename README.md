4c5e3c4101deb2ab1981fc0ba6695a8b14c35d8c
svbachman <sv.bachman@gmail.com>
2020-10-09 23:44:40 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# yt-cart
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/yt-cart)
---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+  return (
+    <div className="grid-container">
+      <header>
+      <a href="/">React Cart </a>
+      </header>
+      <main>
+      Product List
+      </main>
+      <footer>
+      All Rights
+      </footer>
+      
+    </div>
+  );
+}
